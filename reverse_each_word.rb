bc99cb5776d592b186d20b9a7c3d03b559b5e6c5
YanChan  Lin <yonalin1@gmail.com>
2017-10-26 17:04:13 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@ def reverse_each_word (sentence)
     array = sentence.split(/ ? /)
     reversed_words=[]
 array.each do |word|
-  reversed_words << word.reverse 
-  end 
+  reversed_words << word.reverse
+  end
 reversed_words.join(" ")
     # array.collect { |x| x + " " }
     # .reverse

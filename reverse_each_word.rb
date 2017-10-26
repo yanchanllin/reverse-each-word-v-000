bdd9cbad2610037db8270c15613b2e756e1b6bef
YanChan  Lin <yonalin1@gmail.com>
2017-10-26 15:24:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word (sentence)
-  array = []
-  str = sentence
-array << str.split(/,/)
-sentence.each do |word|
-word.reverse
-end
-end
+    array = sentence.split(/  /) 
+    sentence.each do |word|
+    sentence.reverse
+ #  sentence.collect(&:reverse)
+ 
+ end
+end 

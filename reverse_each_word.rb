935e9b09afc7bbb5a78d7ced2563450a4ff063a6
YanChan  Lin <yonalin1@gmail.com>
2017-10-26 17:26:14 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,18 @@
-def reverse_each_word (sentence)
-  array = []
-  str = sentence
-array << str.split(/,/)
-sentence.each do |word|
-word.reverse
-end
-end
+require 'pry'
+#
+# def reverse_each_word(sentence)
+#     array = sentence.split(/ ? /)
+#     reversed_words=[]
+# array.each do |word|
+#   reversed_words << word.reverse
+#   end
+#   reversed_words.join(" ")
+# end
+
+def reverse_each_word(sentence)
+  array = sentence.split(/ ? /)
+  reversed_words=[]
+  reversed_words = array.collect(&:reverse)
+  reversed_words.join(" ")
+ end
+ # sentence.split.collect(&:reverse).join(" ")

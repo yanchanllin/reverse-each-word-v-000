7c8f8a7fbf86bc306e553b24e774c25b0a146835
YanChan  Lin <yonalin1@gmail.com>
2017-10-26 16:25:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def reverse_each_word (sentence)
     array = sentence.split(/ ? /)
-    array.each do |word|.reverse
+    array.each do |word|
+    word.to_i.reverse
     end
 end
  #  sentence.collect(&:reverse)
